bfc1f5c1be6c201a5b40bc19a36fa73e8358f0d2
yz <chulove522@hotmail.com.tw>
2022-09-25 12:51:34 +0800

    0925主視覺
    怪了. 檔名太長上傳不上去-..-

---
+++ Assets/my Shader/BallAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnimation : MonoBehaviour
+{
+    private GameObject rotateobj;
+    private Sprite image;
+    public bool isaBall;
+    private Vector3 rotationvector = new Vector3(0f, 0f, 1f); //icon rotation only has Z axes
+    void Start()
+    {
+        rotateobj = gameObject;
+        if(isaBall)
+            rotationvector = new Vector3(1f, 1f, 1f); //ball rotation has 3 axes
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotateobj.transform.Rotate(rotationvector, 0.5f);
+    }
+}
---
+++ Assets/my Shader/MainGameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameController : MonoBehaviour {
+    [SerializeField]
+    private AudioClip[] audioClips;
+    private AudioSource audioSource;
+
+    void Start() {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        
+    }
+}
