6a78909e160e47e9a97be7360453055a85a3e9cd
Jia-Ye Li <abcd6891@gmail.com>
2022-09-28 00:35:36 +0800

    Add little game Collect Photon demo
    I don't know whey .meta file is renamed, Let it go.

---
+++ Assets/Scripts/LibCollectPhoton/Init2DSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init2DSpeed : MonoBehaviour
+{
+    public Vector2 initSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         Rigidbody2D rb = GetComponent<Rigidbody2D>();
+         rb.velocity = initSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
