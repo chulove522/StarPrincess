ed5dfedeac7f3576475f232c89eee67e96d61e1a
Jia-Ye Li <abcd6891@gmail.com>
2022-10-02 04:53:00 +0800

    Game3 - integrate gamewin

--- Assets/Scripts/LibRemoveBlocking/Game3Flow.cs
+++ Assets/Scripts/LibRemoveBlocking/Game3Flow.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 public class Game3Flow : MonoBehaviour
 {
     public Text TimerText;
+    public Text ScoreText;
     public int MaxGameTime=60;
+    public int StartingTime;
     public MainGameController mainGame;
-    // Start is called before the first frame update
 
     public void Game03Start() {
-        StartCoroutine(showtime());
+        SetStartingTime();
     }
 
     void GameOver() {
@@ -34,19 +35,27 @@ public class Game3Flow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ShowTime();
+        UpdateSocre();
     }
 
+    void UpdateSocre() {
+        int score = (MaxGameTime - StartingTime);
+        if (score < 0)
+            score = 0;
+        Debug.Log(score);
+        ScoreText.text = "Score: " + score.ToString();
+    }
 
-    IEnumerator showtime() {
-        // TODO: condider menu "close" pressed time instread of Time.time
-        // TODO: update showtime per frame
+    void SetStartingTime() {
+        StartingTime = (int) Mathf.Ceil(Time.time);
+        MaxGameTime += StartingTime;
+    }
+
+    void ShowTime() {
         int remaintime = (MaxGameTime - Time.time) > 0 ? (int)(MaxGameTime - Time.time) : 0;
         if (remaintime == 0)
             GameOver();
         TimerText.text = "Time: " + remaintime.ToString();
-        yield return null;
     }
-
-    
 }
