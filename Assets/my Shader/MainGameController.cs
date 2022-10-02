ae64a1b2858894333526206764c251aff57d74d1
yz <chulove522@hotmail.com.tw>
2022-10-03 01:52:50 +0800

    Revert "Fix Space Scene null pointer derefrence"
    This reverts commit bd397c27ed32a73bd57dafc9b5fb439fc4a5c1f7.

--- Assets/my Shader/MainGameController.cs
+++ Assets/my Shader/MainGameController.cs
@@ -7,7 +7,6 @@ using static NasaScript;
 
 public class MainGameController : MonoBehaviour {
     public static MainGameController Instance { get; private set; }
-    public static MainGameController BufferInstance { get; private set; }
     public AudioClip[] audioEffects;
     
     private AudioSource audioSource; //一律掛載在maingameobject上方
@@ -156,17 +155,10 @@ public class MainGameController : MonoBehaviour {
         initTables();
     }
 
-    void updateInstance() {
-        DontDestroyOnLoad(transform.root.gameObject);
-        DontDestroyOnLoad(LoadingInterface.transform.root.gameObject);
-        DontDestroyOnLoad(loadingImg.transform.root.gameObject);
-        BufferInstance = Instance;
-        Instance = this;
-    }
     void Start() {
 
         Debug.Log("start called");
-        updateInstance();
+        DontDestroyOnLoad(this.gameObject);
         hideAll();
 
         audioSource = this.GetComponent<AudioSource>();
@@ -205,7 +197,13 @@ public class MainGameController : MonoBehaviour {
         audioSource.clip = getAudioWithId(nowSceneID);
     }
     void Awake() {
-        updateInstance();
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this) {
+            Destroy(gameObject);
+        }
     }
 
     /// <summary>
@@ -248,6 +246,7 @@ public class MainGameController : MonoBehaviour {
 
     }
     public static void showLoadingScreen(bool show) {
+       
         Instance.LoadingInterface.SetActive(show);
     }
 
