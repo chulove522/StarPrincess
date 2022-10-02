86937c00f8df9941681b1ce33d6ae16c432b98e9
Jia-Ye Li <abcd6891@gmail.com>
2022-10-02 20:04:43 +0800

    Space scene to Dialog scene
    add scritp on button, call related functions

---
+++ Assets/Scripts/SwichSceneHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static MainGameController;
+
+public class SwichSceneHelper : MonoBehaviour
+{
+    public SCENE_ID nextSceneID;
+    public MainGameController controller;
+    // Start is called before the first frame update
+
+    public void onClick()
+    {
+        Debug.Log("next scene ID" + nextSceneID);
+        MainGameController.setTargetScene(nextSceneID);
+        controller.StartGame();
+    }
+}
